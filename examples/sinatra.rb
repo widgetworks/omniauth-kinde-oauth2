fffd79cbea7fc7b6e42321f465e804e1d10f6414
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2024-06-29 18:49:28 +1000

    feat(session): simple session management; allow log out

--- examples/sinatra.rb
+++ examples/sinatra.rb
@@ -8,7 +8,8 @@ require 'omniauth-kinde-oauth2'
 class SinatraKindeApp < Sinatra::Base
 
   configure do
-      set :sessions, true
+    set :sessions, true
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end
 
   use OmniAuth::Builder do
@@ -19,24 +20,61 @@ class SinatraKindeApp < Sinatra::Base
   end
 
   get '/' do
-    <<~HTML
-      <form
-        method="post"
-        action="/auth/kinde_oauth2"
-      >
-          <input
-            type="hidden"
-            name="authenticity_token"
-            value="#{request.env['rack.session']['csrf']}"
-          />
-          <button type='submit'>Login with Kinde</button>
-      </form>
-    HTML
+    if session[:auth_info]
+      redirect to('/logged_in')
+    else
+      erb :index, locals: {
+        request_csrf: request.env['rack.session']['csrf']
+      }
+    end
   end
 
   get '/auth/kinde_oauth2/callback' do
+    # CFH - save token in "session"
+    # redirect to the "logged_in" page
+    # and show the token there
     content_type 'text/plain'
-    request.env['omniauth.auth'].inspect
+    auth_info = request.env['omniauth.auth']
+    session[:auth_info] = auth_info
+
+    # auth_info.inspect
+
+    redirect to('/logged_in')
+  end
+
+  get '/logged_in' do
+    if session[:auth_info]
+      erb :logged_in, :locals => {
+        session: session,
+      }
+    else
+      # clear session?
+      redirect to('/')
+    end
+  end
+
+  post '/log_out' do
+    if session[:auth_info]
+      # do log out
+      kinde_domain = ENV['KINDE_DOMAIN']
+      local_domain = "http://localhost:4567/logged_out"
+      clear_session_auth()
+      redirect "https://#{kinde_domain}/logout?redirect=#{local_domain}"
+    else
+      redirect to('/')
+    end
+  end
+
+  get '/logged_out' do
+    erb :index, locals: {
+      request_csrf: request.env['rack.session']['csrf']
+    }
+  end
+
+  private
+
+  def clear_session_auth
+    session[:auth_info] = nil
   end
 
 end
